e01f9d18b7509e07856984ee97cc08a0cced6d2e
Ryan Sheidow <ryan@crankuptheamps.com>
2018-12-20 11:30:48 +0100

    AC-827 simple python script to build and run tests on linux and windows

---
+++ kerberos/test/build_and_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import platform
+from subprocess import call, check_call
+
+OS = platform.system()
+
+if 'Linux' in OS:
+    OS = 'linux'
+elif 'Windows' in OS:
+    OS = 'windows'
+else:
+    raise RuntimeError('%s is not a supported platform' % OS)
+
+def main():
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    make = 'make' if OS == 'linux' else 'nmake'
+    check_call([make, 'clean'])
+    check_call(make)
+
+    test_suffix = 'Test'
+    if OS == 'windows':
+        test_suffix += '.exe'
+    tests = [f for f in os.listdir('.') if os.path.isfile(f) and f.startswith('AMPS') and f.endswith(test_suffix)]
+    for test in tests:
+        check_call(os.path.join('.', test))
+
+if __name__ == '__main__':
+    main()
+
